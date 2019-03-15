a797936e2089445d9560d1d69d3def79f053f81a
Mateus <a>
2019-03-14 23:45:40 -0300

    Attempt to do the insert functionality but without success.

--- CollegeManager/AngularJsApp/Teacher/Service.js
+++ CollegeManager/AngularJsApp/Teacher/Service.js
@@ -1,5 +1,14 @@
-﻿teacherApp.service('teacherService', function ($http) {
-	this.getAllTeachers = function () {
-		return $http.get("/Teachers/GetTeacher");
-	}
+﻿this.getAllTeachers = function () {
+	return $http.get("/Teachers/GetTeacher");
+}
+
+this.addNewTeacher = function (teacher) {
+	var request = $http({
+		method: 'post',
+		url: '/Teacher/AddNewTeacher',
+		data: teacher
+	});
+
+	return request;
+}
 })
